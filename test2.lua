72030a4458e65ebb8ebebf1e744f2ebc858ee467
longvu26092007-eng <longvu26092007@gmail.com>
2026-03-14 15:12:08 +0700

    Update and rename test1.lua to test2.lua

--- test2.lua
+++ test2.lua
@@ -510,7 +510,7 @@ function CheckLevel()
             NameMon = "Fishman Warrior"
             CFrameQ = CFrame.new(61122.65234375, 18.497442245483, 1569.3997802734)
             CFrameMon = CFrame.new(60844.10546875, 98.462875366211, 1298.3985595703)
-            if _G.tự động cấp and (CFrameMon.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude > 3000 then
+            if _G.AutoLevel and (CFrameMon.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude > 3000 then
                 game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance", Vector3.new(61163.8515625, 11.6796875, 1819.7841796875))
             end
         elseif v7 == 400 or (v7 <= 449 or SelectMonster == "Fishman Commando") then
